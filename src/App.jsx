7f4c82a0ac42afb32556173ca25f3bc57137ebc5
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-20 10:23:47 +0200

    chore: agregar variables personalizadas tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1>Frontend Mentor Challenges</h1>
+      <h1 className="text-steel-gray font-title bg-dark-khaki">Frontend Mentor Challenges</h1>
     </>
   )
 }
