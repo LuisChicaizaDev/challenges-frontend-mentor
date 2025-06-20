2d2f415f0d72e6fe2ebd89658d608186f50a12a2
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-20 11:00:10 +0200

    chore: agregar nuevas fuentes tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-steel-gray font-title bg-dark-khaki">Frontend Mentor Challenges</h1>
+      <h1 className="font-title text-2xl font-bold">Frontend Mentor Challenges</h1>
+      <p className="font-paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio excepturi, minima magni sed ipsa dolores ullam dicta, veritatis in, vel minus ea consequuntur. Beatae aspernatur odit quia molestias, eum earum.</p>
     </>
   )
 }
