db5954f53b061d04730587225e630d4ca04b3f4d
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-23 10:53:37 +0200

    style: eliminar card tecnologias

--- src/assets/components/CardsInfo.jsx
+++ src/assets/components/CardsInfo.jsx
@@ -4,27 +4,15 @@ import { ExternalLink } from 'lucide-react';
 
 export const CardsInfo = () => {
     return (
-        <section className='grid grid-cols-1 gap-4 md:grid-cols-3'>
+        <section className='grid grid-cols-1 gap-6 md:grid-cols-2'>
 
             <div className='flex flex-col gap-4 justify-between p-6 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
                 <div className='flex justify-between'>
                     <h3 className='text-xl dark:text-white'>Desafíos Completados</h3>
-                    <LaptopMinimalCheck className='text-gray-500 size-6 dark:text-gray-400'/>
-                </div>
+                    <CodeXml className='text-gray-500 size-6 dark:text-gray-400'/>                </div>
                 <div>
                     <p className='font-black text-2xl dark:text-white'>2</p>
                     <p className='text-sm text-gray-500 dark:text-gray-400'>Practicando distintos niveles de dificultad</p>
-                </div>
-            </div>
-
-            <div className='flex flex-col gap-4 justify-between p-6 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
-                <div className='flex justify-between'>
-                    <h3 className='text-xl dark:text-white'>Tecnologías Utilizadas</h3>
-                    <CodeXml className='text-gray-500 size-6 dark:text-gray-400'/>
-                </div>
-                <div>
-                    <p className='font-black text-2xl dark:text-white'>10</p>
-                    <p className='text-sm text-gray-500 dark:text-gray-400'>Herramientas de desarrollo y frameworks</p>
                 </div>
             </div>
 
