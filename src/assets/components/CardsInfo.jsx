e376a60c193e14cfca80150489b7d676174dc960
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-23 09:40:39 +0200

    style: modificar size iconos y padding cards info

--- src/assets/components/CardsInfo.jsx
+++ src/assets/components/CardsInfo.jsx
@@ -6,10 +6,10 @@ export const CardsInfo = () => {
     return (
         <section className='grid grid-cols-1 gap-4 md:grid-cols-3'>
 
-            <div className='flex flex-col gap-4 justify-between p-8 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
+            <div className='flex flex-col gap-4 justify-between p-6 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
                 <div className='flex justify-between'>
                     <h3 className='text-xl dark:text-white'>Desafíos Completados</h3>
-                    <LaptopMinimalCheck className='text-gray-500 size-7 dark:text-gray-400'/>
+                    <LaptopMinimalCheck className='text-gray-500 size-6 dark:text-gray-400'/>
                 </div>
                 <div>
                     <p className='font-black text-2xl dark:text-white'>2</p>
@@ -17,10 +17,10 @@ export const CardsInfo = () => {
                 </div>
             </div>
 
-            <div className='flex flex-col gap-4 justify-between p-8 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
+            <div className='flex flex-col gap-4 justify-between p-6 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
                 <div className='flex justify-between'>
                     <h3 className='text-xl dark:text-white'>Tecnologías Utilizadas</h3>
-                    <CodeXml className='text-gray-500 size-7 dark:text-gray-400'/>
+                    <CodeXml className='text-gray-500 size-6 dark:text-gray-400'/>
                 </div>
                 <div>
                     <p className='font-black text-2xl dark:text-white'>10</p>
@@ -28,11 +28,11 @@ export const CardsInfo = () => {
                 </div>
             </div>
 
-            <div className='flex flex-col gap-4 justify-between p-8 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
+            <div className='flex flex-col gap-4 justify-between p-6 bg-white border border-gray-300 rounded-xl dark:bg-slate-800 dark:border-slate-600'>
                 <div className='flex justify-between'>
                     <h3 className='text-xl dark:text-white'>Último Proyecto</h3>
                    <a href="#" target='_blank'>
-                        <ExternalLink className='text-gray-500 size-7 hover:text-gray-400 transition-colors dark:text-gray-400 dark:hover:dark:text-gray-300'/>
+                        <ExternalLink className='text-gray-500 size-6 hover:text-gray-400 transition-colors dark:text-gray-400 dark:hover:dark:text-gray-300'/>
                    </a>
                 </div>
                 <div>
