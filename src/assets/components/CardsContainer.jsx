aa0405e319fdd285a5f7a679769ef03327a27a65
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-24 14:42:27 +0200

    refactor: crear carpeta data de proyectos

--- src/assets/components/CardsContainer.jsx
+++ src/assets/components/CardsContainer.jsx
@@ -1,43 +1,7 @@
 import { Card } from './Card';
-import imageDefault from '../images/default.svg';
+import { projects } from '../data/projects';
 
 export const CardsContainer = () => {
-
-    const projects = [
-        {
-            id: 1,
-            title: 'Proyecto Nuevooo',
-            description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
-            image: imageDefault,
-            technologies: ['HTML', 'CSS', 'JavaScript'],
-            difficulty: 'Novato',
-            demo: '#demo',
-            code: '#code',
-            completed: 'Junio 2025'
-        },
-        {
-            id: 2,
-            title: 'Proyecto Nuevooo',
-            description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
-            image: imageDefault,
-            technologies: ['HTML', 'CSS', 'JavaScript'],
-            difficulty: 'Junior',
-            demo: '#demo',
-            code: '#code',
-            completed: 'Jluio 2025'
-        },
-        {
-            id: 3,
-            title: 'Proyecto Nuevooo',
-            description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
-            image: imageDefault,
-            technologies: ['HTML', 'CSS', 'JavaScript'],
-            difficulty: 'Intermedio',
-            demo: '#demo',
-            code: '#code',
-            completed: 'Jluio 2025'
-        }
-    ];
 
     return (
         <section className="grid grid-cols-1 my-10 md:grid-cols-2 lg:grid-cols-3 gap-6">
---
+++ src/assets/data/projects.js
@@ -0,0 +1,37 @@
+import imageDefault from '../images/default.svg';
+
+export const projects = [
+    {
+        id: 1,
+        title: 'Proyecto Nuevooo',
+        description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
+        image: imageDefault,
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        difficulty: 'Novato',
+        demo: '#demo',
+        code: '#code',
+        completed: 'Junio 2025'
+    },
+    {
+        id: 2,
+        title: 'Proyecto Nuevooo',
+        description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
+        image: imageDefault,
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        difficulty: 'Junior',
+        demo: '#demo',
+        code: '#code',
+        completed: 'Jluio 2025'
+    },
+    {
+        id: 3,
+        title: 'Proyecto Nuevooo',
+        description: 'Prueba json proyectos. Pasar props al componente Card para renderizarlo',
+        image: imageDefault,
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        difficulty: 'Intermedio',
+        demo: '#demo',
+        code: '#code',
+        completed: 'Jluio 2025'
+    }
+];
