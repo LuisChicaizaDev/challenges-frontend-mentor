edac0da43303dd0963f6dafe5ea5826910308a41
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-06-25 10:43:07 +0200

    feat: agregar boton ver todos los desafios

--- challenges/four-card-feature/src/components/Footer.jsx
+++ challenges/four-card-feature/src/components/Footer.jsx
@@ -1,6 +1,10 @@
 export const Footer = () => {
     return (
-        <footer>
+        <footer className="text-center">
+            <a href="https://frontend-mentor-luis.vercel.app/" 
+                className="inline-block my-8 border border-gray-300 py-2 px-4 rounded-md text-center text-sm text-gray-900 hover:bg-gray-100 hover:text-blue-500 transition">
+                Ver todos los desafíos
+            </a>
             <p className="text-center">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="underline underline-offset-2">Frontend Mentor</a>. 
             Coded by <a href="https://github.com/LuisChicaizaDev" target="_blank" className="underline text-blue underline-offset-2 hover:text-blue-500 transition">Luis Chicaiza</a>.
