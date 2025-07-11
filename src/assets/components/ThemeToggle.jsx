a60ecd063713dc8eee9ddf86241b2bed01b9775e
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-07-11 11:26:57 +0200

    style: corregir iconos segun el tema ligth/dark

--- src/assets/components/ThemeToggle.jsx
+++ src/assets/components/ThemeToggle.jsx
@@ -1,4 +1,4 @@
-import { Sun, CloudMoon } from 'lucide-react';
+import { Sun, Moon } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 export const ThemeToggle = () => {
@@ -25,15 +25,15 @@ export const ThemeToggle = () => {
     }
 
     return (
-        <button className='p-1.5 border border-gray-400 rounded-lg cursor-pointer bg-gray-300/80
-        hover:bg-gray-100 transition duration-500 dark:bg-gray-700 dark:hover:bg-gray-900' 
+        <button className='p-1.5 border border-gray-400 rounded-lg cursor-pointer
+        hover:bg-gray-300 transition duration-500 dark:hover:bg-gray-700' 
             aria-label='Alternar tema'
             onClick={toggleTheme}
         >
             { theme === "light" ? 
-                <Sun className='rotate-0 scale-100 transition- dark:-rotate-90 dark:scale-0'/> 
+                <Moon className='dark:-rotate-90 dark:scale-0 transition'/> 
                 :  
-                <CloudMoon color='#ffffff' className='rotate-90 scale-0 transition- dark:rotate-0 dark:scale-100'/> 
+                <Sun color='#fff' className='dark:rotate-90 dark:scale-100 transition'/> 
             } 
            
         </button>
